cd81c9c9ffb751160ae34a7fc5b036e51821268a
alexvotofuture <mrminion123654@hotmail.com>
2018-01-22 10:02:14 -0800

    Update PythonController.py

--- PythonController.py
+++ PythonController.py
@@ -1,7 +1,11 @@
-import time
-import serial
-ser = serial.Serial('COM27', 9600)
-time.sleep(2)
+import serial, time
+arduino = serial.Serial('/dev/cu.usbserial-09HP5727', 115200, timeout=.1)
+time.sleep(1) #give the connection a second to settle
+
 while True:
-    pixelLength = raw_input('How many pixels would you like to light up?: ')
-    ser.write(pixelLength)
+#    data = arduino.readline()
+#    if data:
+#        print data.rstrip('\n') #strip out the new lines for now
+#        # (better to do .read() in the long run for this reason
+    pixelLength = raw_input('How many pixels would you like to illuminate?: ')
+    arduino.write(pixelLength)
