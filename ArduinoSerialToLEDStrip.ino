bb76a55803131d20c35a7a5df79888dc39c9a4a6
alexvotofuture <mrminion123654@hotmail.com>
2018-01-22 01:32:19 -0800

    Update ArduinoSerialToLEDStrip.ino

--- ArduinoSerialToLEDStrip.ino
+++ ArduinoSerialToLEDStrip.ino
@@ -12,10 +12,11 @@ Adafruit_DotStar strip = Adafruit_DotStar(NUMPIXELS, DATAPIN, CLOCKPIN);
 const int potPin = 5;
 int potVal = 0;
 int mapPotVal = 0;
-int serialMessage = 0;
+int pyCommand = 0;
 
 void setup() {
   Serial.begin(9600);
+  Serial.println("Serial Connection Initialized");
   pinMode(potPin, INPUT_PULLUP);
   strip.begin(); // Initialize pins for output
   strip.show();  // Turn all LEDs off ASAP
@@ -23,30 +24,37 @@ void setup() {
 
 // This requires about 200 mA for all the 'on' pixels + 1 mA per 'off' pixel.
 
-//int      head  = 0, tail = -30; // Index of first 'on' and 'off' pixels
-//uint32_t color = 0x000000;      // 'On' color (starts red)
-
 void loop() {
- if (Serial.available() > 0) {
-  serialMessage = (Serial.read(), DEC);
- }
+  if(Serial.available() > 0) {
+    int commandLength = Serial.available();
+    char commandArray[commandLength];
+    for(int i; i<commandLength; i++){
+      char data = Serial.read();   
+      commandArray[i] = data;
+    } 
+    Serial.println(commandArray);  
+    pyCommand = commandArray;
+
+  }
+  
+//  potVal = analogRead(potPin);
+//  mapPotVal = map(potVal, 0, 1023, 0, 59);
+  selectWipe(pyCommand, 5); //Uncomment this to make the led count respond to a serial message sent from Python
+//  selectWipe(mapPotVal, 5); //Uncomment this to make the led count respond to the potentiometer
+}
+
+void selectWipe(char pixelLength, int wait) { //converts mapPotVal to pixelLength
   int r=random(0,255);
   int g=random(0,255);
   int b=random(0,255);
-  potVal = analogRead(potPin);
-  mapPotVal = map(potVal, 0, 1023, 0, 59);
-  selectWipe(strip.Color(r, g, b), serialMessage, 5); //Uncomment this to make the led count respond to a serial message sent from Python
-//  selectWipe(strip.Color(r, g, b), mapPotVal, 5); //Uncomment this to make the led count respond to the potentiometer
-}
-
-void selectWipe(uint32_t c, int pixelLength, int wait) { //converts mapPotVal to pixelLength
+  uint32_t c = strip.Color(r, g, b);
   for(uint16_t i=0; i<pixelLength; i++) {
       strip.setPixelColor(i, c);
       strip.show();
   }
-  for(uint16_t i=pixelLength; i<strip.numPixels(); i++){
-    strip.setPixelColor(i, 0x000000); //make all black
+  for(uint16_t i=pixelLength; i<strip.numPixels(); i++){ //for all remaining, unlit pixels...
+    strip.setPixelColor(i, 0x000000); //set remaining unlit pixels to the color black
     strip.show();
   }
-  delay(wait);
+//  delay(wait);
 }
